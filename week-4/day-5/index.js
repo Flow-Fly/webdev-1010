34107001b45ec0caf48461e54dd43d42c1c69cae
Flo <florian.aube@gmail.com>
2022-11-04 11:56:23 +0100

    Instruction on exercise

---
+++ week-4/day-5/index.js
@@ -0,0 +1,119 @@
+// import mongoose from "mongoose"
+
+const mongoose = require("mongoose")
+
+const model = mongoose.model
+const Schema = mongoose.Schema
+
+const students = [
+	{
+		name: "Bob",
+		campus: "Paris",
+		favoriteLanguage: ["JavaScript", "HTML"],
+	},
+	{
+		name: "Alice",
+		campus: "Barcelona",
+		favoriteLanguage: ["JavaScript", "HTML", "CSS"],
+	},
+	{
+		name: "John",
+		campus: "Miami",
+		favoriteLanguage: ["JavaScript", "Python"],
+	},
+]
+
+const projectSchema = new Schema({
+	module: Number,
+	name: {
+		type: String,
+		match: /[A-Za-z]/,
+	},
+	description: {
+		type: String,
+		maxLength: 500,
+		required: true,
+	},
+})
+
+const studentSchema = new Schema({
+	name: {
+		required: true,
+		type: String,
+		maxLength: 50,
+		match: /[A-Za-z]/,
+	},
+	projects: {
+		type: [Schema.Types.ObjectId],
+		ref: "Project",
+	},
+	campus: {
+		type: String,
+		enum: ["Paris", "Madrid", "Miami", "Barcelona"],
+	},
+	favoriteLanguage: {
+		type: [String],
+		enum: ["JavaScript", "CSS", "HTML", "Python"],
+	},
+})
+
+const Project = model("Project", projectSchema)
+const Student = model("Student", studentSchema)
+
+mongoose
+	.connect("mongodb://localhost/Squads")
+	.then(async (db) => {
+		try {
+			console.log(`Successfully connected to ${db.connection.name}`)
+			await Project.deleteMany()
+			await Student.deleteMany()
+
+			/**
+			 * Creating multiple entries
+			 */
+			const allStudents = await Student.create(students)
+
+			// console.log(allStudents)
+			/**
+			 * Creating one entry
+			 */
+			const myFirstProject = await Project.create({
+				module: 1,
+				name: "AwesumProj",
+				description: "Nicest project ever!",
+			})
+
+			/**
+			 * Find someone with a regular Query
+			 */
+			// const bob = Student.findOne({$or :[{name: 'Bob' }, {name:'Alice'}]} )
+			// const alice = await Student.findById()
+
+			const alice = await Student.findOne({ name: "Alice" }, { name: 1 })
+			// This will get every students from the db
+			// const everyStudent = await Student.find()
+			console.log(alice)
+
+			/**
+			 * Update a document
+			 */
+
+			const updatedBob = await Student.findOneAndUpdate(
+				{ name: "Bob" },
+				{ $push: { projects: myFirstProject._id } },
+				{ new: true }
+			)
+
+			console.log(updatedBob)
+
+			// await Student.findByIdAndDelete(alice._id)
+			// await Student.findOneAndDelete({ _id: alice._id })
+			// await Student.findOneAndDelete({name: 'Alice'})
+			// console.log(returnValue)
+
+			await mongoose.disconnect()
+		} catch (error) {
+			console.error(error)
+		}
+	})
+	.catch((error) => console.error(error))
---
+++ week-4/day-5/instructions.md
@@ -0,0 +1,23 @@
+# Create models / Schema
+
+## Ironhack Team
+
+### Persons
+
+- name
+- age
+
+### Teacher
+
+- personId
+- enum: TA - LT
+
+### Students
+
+- personId
+- attendedBC
+
+- Create the schema
+- Create the models
+- Connect to the DB
+- Create / Update / Delete some informations
